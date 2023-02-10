3fab13fa635d2d23f72d83ce36677f05c861fba1
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-10 09:22:50 -0300

    feat: Adds middleware to the route of creating a customer

--- src/routes/customersRoutes.ts
+++ src/routes/customersRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { createCustomer } from '../controllers/customersController';
+import { validatesSchema } from '../middlewares/validatesSchema';
 
 const customersRouter = Router();
 
-customersRouter.post('/customer');
+customersRouter.post('/customer', validatesSchema("customer") ,createCustomer);
 customersRouter.get('/customers');
 customersRouter.get('/customer/cpf');
 
