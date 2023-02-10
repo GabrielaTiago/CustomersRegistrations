7d8aa246abe9a6e041adfc786d9c3076363cb6a3
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-10 16:25:31 -0300

    feat: Adds the getAllCustomers controller to the 'customers' route

--- src/routes/customersRoutes.ts
+++ src/routes/customersRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { createCustomer, getAllCutomers } from '../controllers/customersController';
+import { createCustomer, getAllCutomers, getCustomerByCPF } from '../controllers/customersController';
 import { validatesSchema } from '../middlewares/validatesSchema';
 
 const customersRouter = Router();
 
 customersRouter.post('/customer', validatesSchema('customer'), createCustomer);
 customersRouter.get('/customers', getAllCutomers);
-customersRouter.get('/customer/cpf');
+customersRouter.get('/customer/:cpf', getCustomerByCPF);
 
 export { customersRouter };
