1f252899f545a3f1d6ccb9dde93bd5ca86157720
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-10 13:58:56 -0300

    feat: Creates server errorHandler middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import 'express-async-errors';
 import cors from 'cors';
 import express, { json } from 'express';
 import { router } from './routes/routes';
+import { errorHandler } from './middlewares/errorHandler';
 
 const server = express();
 
 server.use(cors(), json());
 server.use(router);
+server.use(errorHandler);
 
 export { server };
